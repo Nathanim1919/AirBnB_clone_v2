290b7a1789210e4ce7e92d0d09cb882a8ef7aae6
fawazabdganiyu <fawazabdganiyu@gmail>
2023-11-21 02:03:03 +0100

    fix: Modify the `BaseModel` for sqlachemy class

--- models/base_model.py
+++ models/base_model.py
@@ -1,19 +1,26 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
+import models
 import uuid
 from datetime import datetime
+from sqlalchemy import Column, String, Date
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+
+    id = Column(String(60), primary_key=True, unique=True, nullable=False)
+    created_at = Column(Date, nullable=False, default=datetime.utcnow())
+    updated_at = Column(Date, nullable=False, default=datetime.utcnow())
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        from models import storage
+
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-        if not kwargs:
-            storage.new(self)
         if kwargs:
             for k, v in kwargs.items():
                 if k != '__class__':
@@ -30,7 +37,8 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.new(self)
+        models.storage.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
@@ -40,4 +48,14 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        try:
+            del dictionary['_sa_instance_state']
+        except KeyError:
+            pass
         return dictionary
+
+    def delete(self):
+        """Delete the current instance from the `FileStorage` (models.storage)
+        with its delete method
+        """
+        models.storage.delete(self)
