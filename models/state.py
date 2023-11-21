8ac4c0786e991cf880a8c36d637dce5874b8c03a
fawazabdganiyu <fawazabdganiyu@gmail>
2023-11-21 11:39:35 +0100

    Update `State` class for databse and relationship with `City` class

--- models/state.py
+++ models/state.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from sqlalchemy import (Column, String)
+from sqlalchemy.orm import relationship
+from models.base_model import (BaseModel, Base)
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', backref='state',
+                          cascade='all, delete, delete-orphan')
+
+    @property
+    def cities(self):
+        """Returns the list of `City` class instances attribute
+        """
+        return self.cities
