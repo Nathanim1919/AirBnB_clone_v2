8cce5a84152676dc367c68d6e35b74b2888c3e53
fawazabdganiyu <fawazabdganiyu@gmail>
2023-11-25 22:46:09 +0100

    Add test suites in each `State` and `City` test file

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 """ """
+import MySQLdb
+import os
+import unittest
+from console import HBNBCommand
+from io import StringIO
 from tests.test_models.test_base_model import test_basemodel
+from tests.test_models.test_engine.test_db_storage import get_current
+from unittest.mock import patch
 from models.city import City
 
 
@@ -24,3 +31,47 @@ class test_City(test_basemodel):
         new = self.value()
         new.save()
         self.assertEqual(type(new.name), str)
+
+    @unittest.skipIf((os.getenv('HBNB_TYPE_STORAGE') != 'db'),
+                     "Only database supported")
+    def test_create_more_parameter(self):
+        """
+        Test that a class (table) with more than a parameter is added
+        to the database
+        """
+        count_states_0 = get_current('states')
+        count_city_0 = get_current('cities')
+
+        with patch('sys.stdout', new=StringIO()) as output:
+            HBNBCommand().onecmd('create State name="California"')
+            state_id = output.getvalue().strip()
+
+            HBNBCommand().onecmd(f'create City state_id="{state_id}" \
+                                 name="San_Francisco"')
+
+        count_states_1 = get_current('states')
+        count_city_1 = get_current('cities')
+
+        self.assertEqual(count_states_1, count_states_0 + 1)
+        self.assertEqual(count_city_1, count_city_0 + 1)
+
+    @unittest.skipIf((os.getenv('HBNB_TYPE_STORAGE') != 'db'),
+                     "Only database supported")
+    def test_create_more_param(self):
+        """Test that a value with "_" is parsed and added
+        """
+        count_states_0 = get_current('states')
+        count_city_0 = get_current('cities')
+
+        with patch('sys.stdout', new=StringIO()) as output:
+            HBNBCommand().onecmd('create State name="California"')
+            state_id = output.getvalue().strip()
+
+            HBNBCommand().onecmd(f'create City state_id="{state_id}" \
+                                  name="Fremont"')
+
+        count_states_1 = get_current('states')
+        count_city_1 = get_current('cities')
+
+        self.assertEqual(count_states_1, count_states_0 + 1)
+        self.assertEqual(count_city_1, count_city_0 + 1)
--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 """ """
+import MySQLdb
+import os
+import unittest
+from console import HBNBCommand
+from io import StringIO
 from tests.test_models.test_base_model import test_basemodel
+from tests.test_models.test_engine.test_db_storage import get_current
+from unittest.mock import patch
 from models.state import State
 
 
@@ -18,3 +25,32 @@ class test_state(test_basemodel):
         new = self.value()
         new.save()
         self.assertEqual(type(new.name), str)
+
+    def test_create_kwargs_success(self):
+        """
+        Test that create method with one keyword argument do not end
+        the interpreter
+        """
+        self.assertFalse(HBNBCommand()
+                         .onecmd('create State name="California"'))
+
+    def test_create_kwargs_none(self):
+        """
+        Test that none value raises an exception
+        """
+        with self.assertRaises(NameError):
+            HBNBCommand().onecmd('create User name=none')
+
+    @unittest.skipIf((os.getenv('HBNB_TYPE_STORAGE') != 'db'),
+                     "Only database supported")
+    def test_create_one_parameter(self):
+        """Test that a new instance is created and added to the database
+        """
+        count_0 = get_current('states')
+
+        with patch('sys.stdout', new=StringIO()):
+            HBNBCommand().onecmd('create State name="California"')
+
+        count_1 = get_current('states')
+
+        self.assertEqual(count_1, count_0 + 1)
