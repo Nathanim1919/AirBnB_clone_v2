ae6a28627ce06e78dbf0611ed78a4a4f182daafa
fawazabdganiyu <fawazabdganiyu@gmail>
2023-12-07 17:08:33 +0100

    Refactor `do_deploy` for proper return value

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -48,8 +48,7 @@ def do_deploy(archive_path):
     if not os.path.exists(archive_path):
         return False
     # Copy the compressed file to the remote server
-    status = put(archive_path, "/tmp/")
-    if status.failed is True:
+    if put(archive_path, "/tmp/").failed:
         return False
 
     # Get `archive_path` without the extension
@@ -58,45 +57,37 @@ def do_deploy(archive_path):
 
     # Make the destination directory
     destination_path = '/data/web_static/releases/'
-    status = run("mkdir -p {}{}/".format(destination_path, archive_name))
-    if status.failed is True:
+    if run("mkdir -p {}{}/".format(destination_path, archive_name)).failed:
         return False
 
     # Uncompress the file on the remote server
     command = "tar -xzf {} -C {}"
     compressed_file = "/tmp/{}".format(compressed_file)
-    archive_name_with_path = "{}{}/".format(destination_path, archive_name)
-    status = run(command.format(compressed_file, archive_name_with_path))
-    if status.failed is True:
+    full_archive_name_dir = "{}{}/".format(destination_path, archive_name)
+    if run(command.format(compressed_file, full_archive_name_dir)).failed:
         return False
 
     # Remove the compressed file from where it was initially copied to
-    status = run("rm {}".format(compressed_file))
-    if status.failed is True:
+    if run("rm {}".format(compressed_file)).failed:
         return False
 
     # Move the uncompressed files to the appropriate location for serving
-    status = run("mv {}web_static/* {}".format(archive_name_with_path,
-                                               archive_name_with_path))
-    if status.failed is True:
+    if run("mv {0}web_static/* {0}".format(full_archive_name_dir)).failed:
         return False
 
     # Remove `web_static` directory in the `destination_path`
-    status = run("rm -rf {}web_static".format(archive_name_with_path))
-    if status.failed is True:
+    if run("rm -rf {}web_static".format(full_archive_name_dir)).failed:
         return False
 
     # Remove precious created symbolic link for testing
-    status = run("rm -rf /data/web_static/current")
-    if status.failed is True:
+    if run("rm -rf /data/web_static/current").failed:
         return False
 
     # Create a new the symbolic link, linked to the new version of your code
-    target = archive_name_with_path
+    target = full_archive_name_dir
     link = '/data/web_static/current'
-    status = run("ln -s {} {}".format(target, link))
-    if status.succeeded is True:
-        print("New version deployed!")
-        return True
-    else:
+    if run("ln -s {} {}".format(target, link)).failed:
         return False
+
+    print("New version deployed!")
+    return True
