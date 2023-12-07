23f9ea7196ce1a34fe7fe51f15ff602011171f2a
fawazabdganiyu <fawazabdganiyu@gmail>
2023-12-07 12:05:22 +0100

    Compress `web_static` directory to be deployed

---
+++ 1-pack_web_static.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Module Name: 1-pack_web_static.py
+Description: Provides definition of `do_pack` function
+"""
+import os
+from datetime import datetime
+from fabric.api import env, local
+
+
+def get_filename():
+    """
+    Get the filename format
+    """
+    formatted_date = datetime.now().strftime("%Y%m%d%H%M%S")
+    filename = f"web_static_{formatted_date}.tgz"
+
+    return filename
+
+
+def do_pack():
+    """
+    Compress web_static directory
+    """
+    os.makedirs("versions", exist_ok=True)
+    filename = get_filename()
+    archive_path = f"versions/{filename}"
+
+    print("Packing web_static to", archive_path)
+    command = "tar -cvzf {} web_static"
+    status = local(command.format(archive_path))
+
+    archive_size = os.path.getsize(archive_path)
+    print("web_static packed: {} -> {}Bytes".format(archive_path,
+                                                    archive_size))
+
+    return archive_path if status.succeeded is True else None
