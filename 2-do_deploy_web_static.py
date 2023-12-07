8f693ae69b519a76c02a4cdc932a8f93e8f3d932
fawazabdganiyu <fawazabdganiyu@gmail>
2023-12-07 17:27:37 +0100

    fix: handle error with exception handling

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -45,46 +45,42 @@ def do_deploy(archive_path):
     """
     if not os.path.exists(archive_path):
         return False
-    # Copy the compressed file to the remote server
-    if put(archive_path, "/tmp/").failed:
-        return False
 
-    # Get `archive_path` without the extension
-    compressed_file = archive_path.split("/")[1]
-    archive_name = compressed_file.split(".")[0]
+    try:
+        # Copy the compressed file to the remote server
+        put(archive_path, "/tmp/")
 
-    # Make the destination directory
-    destination_path = '/data/web_static/releases/'
-    if run("mkdir -p {}{}/".format(destination_path, archive_name)).failed:
-        return False
+        # Get `archive_path` without the extension
+        compressed_file = archive_path.split("/")[1]
+        archive_name = compressed_file.split(".")[0]
 
-    # Uncompress the file on the remote server
-    command = "tar -xzf {} -C {}"
-    compressed_file = "/tmp/{}".format(compressed_file)
-    full_archive_name_dir = "{}{}/".format(destination_path, archive_name)
-    if run(command.format(compressed_file, full_archive_name_dir)).failed:
-        return False
+        # Make the destination directory
+        destination_path = '/data/web_static/releases/'
+        run("mkdir -p {}{}/".format(destination_path, archive_name))
 
-    # Remove the compressed file from where it was initially copied to
-    if run("rm {}".format(compressed_file)).failed:
-        return False
+        # Uncompress the file on the remote server
+        command = "tar -xzf {} -C {}"
+        compressed_file = "/tmp/{}".format(compressed_file)
+        full_archive_name_dir = "{}{}/".format(destination_path, archive_name)
+        run(command.format(compressed_file, full_archive_name_dir))
 
-    # Move the uncompressed files to the appropriate location for serving
-    if run("mv {0}web_static/* {0}".format(full_archive_name_dir)).failed:
-        return False
+        # Remove the compressed file from where it was initially copied to
+        run("rm {}".format(compressed_file))
 
-    # Remove `web_static` directory in the `destination_path`
-    if run("rm -rf {}web_static".format(full_archive_name_dir)).failed:
-        return False
+        # Move the uncompressed files to the appropriate location for serving
+        run("mv {0}web_static/* {0}".format(full_archive_name_dir))
 
-    # Remove precious created symbolic link for testing
-    if run("rm -rf /data/web_static/current").failed:
-        return False
+        # Remove `web_static` directory in the `destination_path`
+        run("rm -rf {}web_static".format(full_archive_name_dir))
 
-    # Create a new the symbolic link, linked to the new version of your code
-    target = full_archive_name_dir
-    link = '/data/web_static/current'
-    if run("ln -s {} {}".format(target, link)).failed:
+        # Remove precious created symbolic link for testing
+        run("rm -rf /data/web_static/current")
+
+        # Create a new the symbolic link, linked to the new version of the code
+        target = full_archive_name_dir
+        link = '/data/web_static/current'
+        run("ln -s {} {}".format(target, link))
+    except Exception:
         return False
 
     print("New version deployed!")
