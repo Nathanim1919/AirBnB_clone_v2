ea7fab7c34c7dce7c1b182bfada4c88d56085dd0
fawazabdganiyu <fawazabdganiyu@gmail>
2023-12-07 02:40:33 +0100

    Add other test files

--- test_main/main_place_amenities.py
+++ test_main/main_place_amenities.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """ Test link Many-To-Many Place <> Amenity
 """
-from models import *
+import models
 
 # creation of a State
-state = State(name="California")
-state.save()
+#state = State(name="California")
+#state.save()
 
 # creation of a City
 city = City(state_id=state.id, name="San Francisco")
