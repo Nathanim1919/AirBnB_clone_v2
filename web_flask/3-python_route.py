ef605aa7fea9029e603402e0f340b4cdc934b2c9
Nathanim1919 <nathanim2tadele@gmail.com>
2024-01-28 01:36:10 -0800

    python is fun

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -26,6 +26,7 @@ def c_text(text):
     return "C {}".format(text)
 
 
+@app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python_text(text="is cool"):
     """display “C ”, followed by the value of the text variable"""
