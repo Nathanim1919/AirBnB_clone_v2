548a6dbae5c3180b764108e0c0e6b120b199cfec
Nathanim1919 <nathanim2tadele@gmail.com>
2024-01-28 01:22:55 -0800

    c is fun

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -22,6 +22,7 @@ def hbnb():
 @app.route("/c/<text>", strict_slashes=False)
 def c_text(text):
     """display 'C ' followed by the value of the text variable"""
+    text = text.replace('_', ' ')
     return "C {}".format(text)
 
 
