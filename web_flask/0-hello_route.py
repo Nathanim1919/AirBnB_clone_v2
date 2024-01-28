0d2ffc1671b9128d3626d2d447b595ec9390ff6e
Nathanim1919 <nathanim2tadele@gmail.com>
2024-01-28 00:59:58 -0800

    bug fixed

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-Start a simple Flask web application.
-"""
+""" 0. Script to start a Flask web application """
 
 from flask import Flask
 
@@ -9,13 +7,10 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def hello_hbnb():
-    """
-    Display "Hello HBNB!" when the root URL is accessed.
-    """
-    return "HELLO HBNB!"
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """ Returns some text. """
+    return 'Hello HBNB!'
 
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
